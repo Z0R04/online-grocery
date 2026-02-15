a06bff394a4b21ca7e55d585ec0ccc4ee160c33e
unknown <angelakattel989@gmail.com>
2026-02-15 20:44:53 +0545

    adding the update function

--- js/app.js
+++ js/app.js
@@ -5,21 +5,27 @@ let items = groceryItems;
 
 import { createForm } from "./form.js";
 
+let editId = null;
+
 // Render App
 function render() {
   const app = document.getElementById("app");
-  app.innerHTML = "";
+  app.innerHTML = " ";
 
-  const formElement = createForm();
+  const formElement = createForm(
+    editId,
+    editId ? items.find((item) => item.id === editId) : null,
+  ); // edited line
   const itemsElement = createItems(items);
 
-  app.appendChild(formElement);
-  app.appendChild(itemsElement);
+  app.append(formElement);
+  app.append(itemsElement);
 }
 render();
+
 // Generate unique ID
 function generateId() {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
 }
 
 // Add Item Function
@@ -54,4 +60,15 @@ export function deleteItem(id) {
   setTimeout(() => {
     alert("Item deleted successfully!");
   }, 0);
+}
+export function updateItemName(newName) {
+  items = items.map((item) => {
+    if (item.id === editId) {
+      return { ...item, name: newName };
+    }
+    return item;
+  });
+  editId = null;
+  render();
+  setTimeout(() => alert("Item Updated Successfully!"), 0);
 }
