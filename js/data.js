511b61f79647e3f63b72ba3cb5e3a6ec6c751af1
unknown <angelakattel989@gmail.com>
2026-02-17 22:10:21 +0545

    list deletig

--- js/data.js
+++ js/data.js
@@ -1,6 +1,1 @@
-export const groceryItems = [
-  { id: "1", name: "milk", completed: true },
-  { id: "2", name: "bread", completed: true },
-  { id: "3", name: "eggs", completed: false },
-  { id: "4", name: "butter", completed: false },
-];
+export const groceryItems = [];
